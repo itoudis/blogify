e2c1ceab819be07ef6b7412f9db5584f547fafc6
EmreBasar98 <emre.basar.98@gmail.com>
2023-05-18 19:28:37 +0300

    first commit

---
+++ blogify-server/src/main/java/com/itoudis/blogifyserver/BlogifyServerApplication.java
@@ -0,0 +1,13 @@
+package com.itoudis.blogifyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogifyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogifyServerApplication.class, args);
+    }
+
+}
